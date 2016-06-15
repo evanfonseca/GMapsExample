bd4a5f071b7fd9534bbf17dbe39a5811b973b14a
Dev02 <evanfonseca@gmail.com>
2016-06-15 14:43:02 -0100

    adding marker onclick

--- app/src/main/java/com/example/dev02/gmapsexample/MapsFragment.java
+++ app/src/main/java/com/example/dev02/gmapsexample/MapsFragment.java
@@ -1,7 +1,12 @@
 package com.example.dev02.gmapsexample;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -14,6 +19,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.security.Provider;
+
 public class MapsFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
@@ -22,6 +29,11 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getMapAsync(this);
+
+
+
+
+
     }
 
     @Override
@@ -31,6 +43,9 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.setOnMapClickListener(this);
 
+        //Location l=mMap.getMyLocation();
+        //l.toString();
+        //Toast.makeText(getContext(),"Coordenadas: "+l.toString(),Toast.LENGTH_LONG).show();
 
         if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
@@ -47,10 +62,60 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         marker.title("Marker Cabo Verde");
         mMap.addMarker(marker);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(caboverde));
+
+
+
+
+
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        // Creating an empty criteria object
+
+        Criteria criteria = new Criteria();
+
+        // Getting the name of the provider that meets the criteria
+
+        String provider = locationManager.getBestProvider(criteria, false);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+
+        double latitude = location.getLatitude();
+
+        double longitude = location.getLongitude();
+
+        Toast.makeText(getContext(),"Minha Localização é lat: "+latitude+" long: "+longitude,Toast.LENGTH_LONG).show();
+
+
+
+
+
+
     }
 
     @Override
     public void onMapClick(LatLng latLng) {
+
+        mMap.clear();
         Toast.makeText(getContext(),"Coordenadas: "+latLng.toString(),Toast.LENGTH_LONG).show();
+
+
+
+        LatLng caboverde = new LatLng(latLng.latitude, latLng.longitude);
+        MarkerOptions marker= new MarkerOptions();
+        marker.position(caboverde);
+        marker.title("Selecionado ");
+        mMap.addMarker(marker);
+
     }
 }
